e26b565709a46896177acd179105291b679969e2
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2023-07-22 17:23:17 +0200

    added new files to the server

--- src-server/SporglooServer.dpr
+++ src-server/SporglooServer.dpr
@@ -1,17 +1,33 @@
 program SporglooServer;
 
 {$APPTYPE CONSOLE}
-
 {$R *.res}
 
 uses
-  System.SysUtils;
+  System.SysUtils,
+  uListeningServer in 'uListeningServer.pas',
+  uConnectedClient in 'uConnectedClient.pas',
+  uAPIMessages in '..\src-common-api\uAPIMessages.pas';
+
+var
+  GameServer: TListeningServer;
 
 begin
+{$IFDEF DEBUG}
+  ReportMemoryLeaksOnShutdown := true;
+{$ENDIF}
   try
-    { TODO -oUser -cConsole Main : Insérer du code ici }
+    GameServer := TListeningServer.Create('127.0.0.1', 80);
+    try
+      // TODO : change IP and port depending on the server
+      // TODO : check if IPv6 is available and open a listening server for it too
+      readln;
+    finally
+      GameServer.Terminate;
+    end;
   except
     on E: Exception do
       Writeln(E.ClassName, ': ', E.Message);
   end;
+
 end.
