dd620b5c667967aa5562eb10065c4e9214de6827
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2023-07-22 23:58:40 +0200

    added a common unit for constants and move constants from others

---
+++ src-common-api/Sporgloo.Consts.pas
@@ -0,0 +1,17 @@
+unit Sporgloo.Consts;
+
+interface
+
+const
+  CSporglooTileNone = 0;
+  CSporglooTilePath = 1;
+  CSporglooTileStar = 2;
+
+  CSporglooAPIMessageTerminator = 0;
+  CSporglooAPIBufferLength = 255;
+
+  CSporglooTileSize = 32;
+
+implementation
+
+end.
