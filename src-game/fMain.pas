4eb864ba6315d7ebc937a9c46272742a2936f0e4
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2023-07-23 00:01:39 +0200

    changed main form create and resize events connect to the server and starts a dialog

--- src-game/fMain.pas
+++ src-game/fMain.pas
@@ -11,10 +11,9 @@ uses
 type
   TfrmMain = class(TForm)
     procedure FormCreate(Sender: TObject);
-    procedure FormDestroy(Sender: TObject);
+    procedure FormResize(Sender: TObject);
   private
     { Déclarations privées }
-    FAPIClient: tSporglooAPIClient;
   public
     { Déclarations publiques }
   end;
@@ -26,21 +25,59 @@ implementation
 
 {$R *.fmx}
 
+uses
+  System.Math,
+  uConfig,
+  uGameData, Sporgloo.Consts;
+
 procedure TfrmMain.FormCreate(Sender: TObject);
 begin
-
+{$IFDEF RELEASE}
+  FullScreen := true;
+{$ENDIF}
+  //
   tthread.ForceQueue(nil,
     procedure
+    var
+      DeviceID, PlayerID: string;
+      GameData: TGameData;
     begin
-      FAPIClient := tSporglooAPIClient.Create('127.0.0.1', 80);
+      GameData := TGameData.Current;
+      GameData.APIClient := tSporglooAPIClient.Create('127.0.0.1', 80);
       // TODO : update IP+Port depending on server infos
+
       // TODO : if connexion failed, retry after a user confirmation or close the program
+              sleep(200);
+      DeviceID := GameData.Player.DeviceID;
+      if DeviceID.IsEmpty then
+        raise Exception.Create('Unknow device ID !');
+
+      PlayerID := GameData.Player.PlayerID;
+      if PlayerID.IsEmpty then
+        GameData.APIClient.SendClientRegister(DeviceID)
+      else
+        GameData.APIClient.SendClientLogin(DeviceID, PlayerID);
     end);
 end;
 
-procedure TfrmMain.FormDestroy(Sender: TObject);
+procedure TfrmMain.FormResize(Sender: TObject);
+var
+  GameData: TGameData;
+  W, H: single;
 begin
-  FAPIClient.Free;
+  GameData := TGameData.Current;
+  W := clientwidth / CSporglootilesize;
+  H := ClientHeight / CSporglootilesize;
+  GameData.Session.MapRangeX := GameData.Player.PlayerX - ceil(W);
+  GameData.Session.MapRangey := GameData.Player.Playery - ceil(H);
+  GameData.Session.MapRangeColNumber := ceil(W);
+  GameData.Session.MapRangeRowNumber := ceil(H);
 end;
+
+initialization
+
+{$IFDEF DEBUG}
+  ReportMemoryLeaksOnShutdown := true;
+{$ENDIF}
 
 end.
