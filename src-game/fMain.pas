1b90aa2b1674b68ed2f4cf97993ca418b02b099e
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2023-07-22 17:24:07 +0200

    added the client socket to the game

--- src-game/fMain.pas
+++ src-game/fMain.pas
@@ -3,13 +3,18 @@ unit fMain;
 interface
 
 uses
-  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
-  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs;
+  System.SysUtils, System.Types, System.UITypes, System.Classes,
+  System.Variants,
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, uClientSocket,
+  FMX.Edit, FMX.Controls.Presentation, FMX.StdCtrls;
 
 type
   TfrmMain = class(TForm)
+    procedure FormCreate(Sender: TObject);
+    procedure FormDestroy(Sender: TObject);
   private
     { Déclarations privées }
+    FAPIClient: tSporglooAPIClient;
   public
     { Déclarations publiques }
   end;
@@ -20,5 +25,22 @@ var
 implementation
 
 {$R *.fmx}
+
+procedure TfrmMain.FormCreate(Sender: TObject);
+begin
+
+  tthread.ForceQueue(nil,
+    procedure
+    begin
+      FAPIClient := tSporglooAPIClient.Create('127.0.0.1', 80);
+      // TODO : update IP+Port depending on server infos
+      // TODO : if connexion failed, retry after a user confirmation or close the program
+    end);
+end;
+
+procedure TfrmMain.FormDestroy(Sender: TObject);
+begin
+  FAPIClient.Free;
+end;
 
 end.
