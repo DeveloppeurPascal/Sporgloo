0ae7cd5a64eecd5540f8c7ee07fdbe8dc9c87143
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2023-07-22 17:58:00 +0200

    added SendMessageX() to server methods

--- src-game/uClientSocket.pas
+++ src-game/uClientSocket.pas
@@ -16,6 +16,8 @@ type
     FSocket: Tsocket;
     procedure Execute; override;
     procedure ReceivedAPIMessage(Const Msg: TSporglooAPIMessage);
+    procedure SendAPIMessage(Const Msg: TSporglooAPIMessage);
+
     procedure onClientRegisterResponse(Const DeviceID,
       PlayerID: TSporglooAPIAlpha16);
     procedure onClientLoginResponse(Const DeviceID,
@@ -28,6 +30,18 @@ type
       NewStarY: TSporglooAPINumber);
     procedure onOtherPlayerMove(Const PlayerID: TSporglooAPIAlpha16;
       Const PlayerX, PlayerY: TSporglooAPINumber);
+
+    procedure SendClientRegister(Const DeviceID: string;
+      var Msg: TSporglooAPIMessage);
+    procedure SendClientLogin(Const DeviceID, PlayerID: string;
+      var Msg: TSporglooAPIMessage);
+    procedure SendMapRefresh(Const MapX, MapY, ColNumber,
+      RowNumber: TSporglooAPINumber; var Msg: TSporglooAPIMessage);
+    procedure SendPlayerMove(Const SessionID, PlayerID: string;
+      Const PlayerX, PlayerY: TSporglooAPINumber; var Msg: TSporglooAPIMessage);
+    procedure SendPlayerPutAStar(Const SessionID, PlayerID: string;
+      Const NewStarX, NewStarY: TSporglooAPINumber;
+      var Msg: TSporglooAPIMessage);
   public
     constructor Create(AIP: string; APort: word);
   end;
@@ -142,6 +156,67 @@ begin
     raise Exception.Create('Message ' + Msg.MessageID.Tostring +
       ' received by the game.');
   end;
+end;
+
+procedure TSporglooAPIClient.SendAPIMessage(const Msg: TSporglooAPIMessage);
+begin
+  // TODO : à compléter
+end;
+
+procedure TSporglooAPIClient.SendClientLogin(const DeviceID, PlayerID: string;
+  var Msg: TSporglooAPIMessage);
+begin
+  Msg.Clear;
+  Msg.MessageID := 3;
+  StringToAlpha16(DeviceID, Msg.Msg3DeviceID);
+  StringToAlpha16(PlayerID, Msg.Msg3PlayerID);
+  SendAPIMessage(Msg);
+end;
+
+procedure TSporglooAPIClient.SendClientRegister(const DeviceID: string;
+  var Msg: TSporglooAPIMessage);
+begin
+  Msg.Clear;
+  Msg.MessageID := 1;
+  StringToAlpha16(DeviceID, Msg.Msg1DeviceID);
+  SendAPIMessage(Msg);
+end;
+
+procedure TSporglooAPIClient.SendMapRefresh(const MapX, MapY, ColNumber,
+  RowNumber: TSporglooAPINumber; var Msg: TSporglooAPIMessage);
+begin
+  Msg.Clear;
+  Msg.MessageID := 5;
+  Msg.Msg5MapX := MapX;
+  Msg.Msg5MapY := MapY;
+  Msg.Msg5ColNumber := ColNumber;
+  Msg.Msg5RowNumber := RowNumber;
+  SendAPIMessage(Msg);
+end;
+
+procedure TSporglooAPIClient.SendPlayerMove(const SessionID, PlayerID: string;
+  const PlayerX, PlayerY: TSporglooAPINumber; var Msg: TSporglooAPIMessage);
+begin
+  Msg.Clear;
+  Msg.MessageID := 7;
+  StringToAlpha16(SessionID, Msg.Msg7SessionID);
+  StringToAlpha16(PlayerID, Msg.Msg7PlayerID);
+  Msg.Msg7PlayerX := PlayerX;
+  Msg.Msg7Playery := PlayerY;
+  SendAPIMessage(Msg);
+end;
+
+procedure TSporglooAPIClient.SendPlayerPutAStar(const SessionID,
+  PlayerID: string; const NewStarX, NewStarY: TSporglooAPINumber;
+  var Msg: TSporglooAPIMessage);
+begin
+  Msg.Clear;
+  Msg.MessageID := 9;
+  StringToAlpha16(SessionID, Msg.Msg9SessionID);
+  StringToAlpha16(PlayerID, Msg.Msg9PlayerID);
+  Msg.Msg9NewStarX := NewStarX;
+  Msg.Msg9NewStarY := NewStarY;
+  SendAPIMessage(Msg);
 end;
 
 end.
