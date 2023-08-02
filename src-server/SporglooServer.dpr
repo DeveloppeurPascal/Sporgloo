30fe768acf78e90dc5d0b84ace2512dbc218dcdc
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2023-08-02 19:44:08 +0200

    changed listened IP address on local computer for the server

--- src-server/SporglooServer.dpr
+++ src-server/SporglooServer.dpr
@@ -10,7 +10,8 @@ uses
   Sporgloo.Database in '..\src-common-api\Sporgloo.Database.pas',
   Sporgloo.Server in 'Sporgloo.Server.pas',
   Sporgloo.Consts in '..\src-common-api\Sporgloo.Consts.pas',
-  Olf.Net.Socket.Messaging in '..\lib-externes\Socket-Messaging-Library\src-library\Olf.Net.Socket.Messaging.pas';
+  Olf.Net.Socket.Messaging
+    in '..\lib-externes\Socket-Messaging-Library\src-library\Olf.Net.Socket.Messaging.pas';
 
 var
   GameServer: TSporglooServer;
@@ -21,13 +22,14 @@ begin
 {$ENDIF}
   try
     // LocalHost
-    // GameServer := TSporglooServer.Create('127.0.0.1', 80);
+//    GameServer := TSporglooServer.Create('127.0.0.1', 8080);
     // Sporgloo VPS (temporary address)
-    // GameServer := TSporglooServer.Create('141.94.221.190', 80);
-    GameServer := TSporglooServer.Create('0.0.0.0', 8080);
+//     GameServer := TSporglooServer.Create('141.94.221.190', 8080);
+     GameServer := TSporglooServer.Create('0.0.0.0', 8080);
     try
       // TODO : change IP and port depending on the server
       // TODO : check if IPv6 is available and open a listening server for it too
+      GameServer.Listen;
       readln;
     finally
       GameServer.Free;
@@ -36,4 +38,5 @@ begin
     on E: Exception do
       Writeln(E.ClassName, ': ', E.Message);
   end;
+
 end.
