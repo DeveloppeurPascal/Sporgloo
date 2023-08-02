962ae4c956b5a99dc9bd1f1fd11b01833febde6a
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2023-08-02 18:18:48 +0200

    rewritten server with Delphi Socket Messaging Library (#32)

--- src-server/SporglooServer.dpr
+++ src-server/SporglooServer.dpr
@@ -5,16 +5,15 @@ program SporglooServer;
 
 uses
   System.SysUtils,
-  uListeningServer in 'uListeningServer.pas',
-  uConnectedClient in 'uConnectedClient.pas',
   Sporgloo.API.Messages in '..\src-common-api\Sporgloo.API.Messages.pas',
   Sporgloo.Types in '..\src-common-api\Sporgloo.Types.pas',
   Sporgloo.Database in '..\src-common-api\Sporgloo.Database.pas',
-  uServerData in 'uServerData.pas',
-  Sporgloo.Consts in '..\src-common-api\Sporgloo.Consts.pas';
+  Sporgloo.Server in 'Sporgloo.Server.pas',
+  Sporgloo.Consts in '..\src-common-api\Sporgloo.Consts.pas',
+  Olf.Net.Socket.Messaging in '..\lib-externes\Socket-Messaging-Library\src-library\Olf.Net.Socket.Messaging.pas';
 
 var
-  GameServer: TListeningServer;
+  GameServer: TSporglooServer;
 
 begin
 {$IFDEF DEBUG}
@@ -22,20 +21,19 @@ begin
 {$ENDIF}
   try
     // LocalHost
-    // GameServer := TListeningServer.Create('127.0.0.1', 80);
+    // GameServer := TSporglooServer.Create('127.0.0.1', 80);
     // Sporgloo VPS (temporary address)
-//    GameServer := TListeningServer.Create('141.94.221.190', 80);
-        GameServer := TListeningServer.Create('0.0.0.0', 8080);
+    // GameServer := TSporglooServer.Create('141.94.221.190', 80);
+    GameServer := TSporglooServer.Create('0.0.0.0', 8080);
     try
       // TODO : change IP and port depending on the server
       // TODO : check if IPv6 is available and open a listening server for it too
       readln;
     finally
-      GameServer.Terminate;
+      GameServer.Free;
     end;
   except
     on E: Exception do
       Writeln(E.ClassName, ': ', E.Message);
   end;
-
 end.
