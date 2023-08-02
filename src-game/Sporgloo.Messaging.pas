ccb4ea701412e6224d1b3949869ad38a34d7ea5f
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2023-08-02 19:40:46 +0200

    updated messages list for local notifications

--- src-game/Sporgloo.Messaging.pas
+++ src-game/Sporgloo.Messaging.pas
@@ -4,27 +4,27 @@ interface
 
 uses
   System.Messaging,
-  uClientSocket,
-  Sporgloo.Types;
+  Sporgloo.Types,
+  Sporgloo.Client;
 
 type
   /// <summary>
   /// Sent when the client is connected to a server
   /// </summary>
-  TServerConnectedMessage = class(TMessage<TSporglooAPIClient>)
+  TServerConnectedMessage = class(TMessage<TSporglooClient>)
   end;
 
   /// <summary>
   /// Connection to the server has been lost
   /// </summary>
-  TLostServerMessage = class(TMessage<TSporglooAPIClient>)
+  TLostServerMessage = class(TMessage<TSporglooClient>)
   end;
 
   /// <summary>
   /// This message is sent by the client/server API to send
   /// the new TileID to the subscribers
   /// </summary>
-  TMapCellUpdateMessage = class(TMessage<TSporglooMapCell>)
+  TMapCellUpdateMessage = class(TObjectMessage<TSporglooMapCell>)
   end;
 
   /// <summary>
