188bce4d5b4dbd919dc3398c44f2afee115d4343
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2024-03-30 13:01:34 +0100

    added current API and an errors list

--- src-common-api/Sporgloo.Consts.pas
+++ src-common-api/Sporgloo.Consts.pas
@@ -3,6 +3,8 @@ unit Sporgloo.Consts;
 interface
 
 const
+  CAPIVersion = 20240330;
+
   CSporglooTileNone = 0;
   CSporglooTilePath = 1;
   CSporglooTileStar = 2;
@@ -10,6 +12,13 @@ const
   CSporglooTileSize = 64;
 
   CSporglooIDSize = 16;
+
+type
+{$SCOPEDENUMS ON}
+  TSporglooErrorCode = (NoError = 0, WrongDeviceID = 1, WrongPlayerID = 2,
+    WrongSessionID = 3, WrongAPIVersion = 4, UnknowPlayerID = 5,
+    WrongDeviceForPlayerID = 6, UnknowSessionID = 7,
+    WrongDeviceOrPlayerForSessionID = 8, WrongMessageReceived = 9);
 
 implementation
 
