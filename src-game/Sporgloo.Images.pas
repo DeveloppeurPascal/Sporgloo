f9b7092d01a81cdab96cc0ab2fa4b5979c4de29b
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2024-05-08 15:57:44 +0200

    added new images in the list

--- src-game/Sporgloo.Images.pas
+++ src-game/Sporgloo.Images.pas
@@ -3,7 +3,10 @@ unit Sporgloo.Images;
 interface
 
 uses
-  System.SysUtils, System.Classes, System.ImageList, FMX.ImgList;
+  System.SysUtils,
+  System.Classes,
+  System.ImageList,
+  FMX.ImgList;
 
 type
   TdmSporglooImages = class(TDataModule)
@@ -21,7 +24,6 @@ var
 implementation
 
 {%CLASSGROUP 'FMX.Controls.TControl'}
-
 {$R *.dfm}
 
 end.
