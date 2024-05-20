293e023b420b1d61cf82cfacf5038eb1146a1974
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2024-05-20 18:39:08 +0200

    replaced bitmap images by SVG images (#72)

--- src-game/cLifeLevel.pas
+++ src-game/cLifeLevel.pas
@@ -14,7 +14,6 @@ uses
   FMX.Forms,
   FMX.Dialogs,
   FMX.StdCtrls,
-  Sporgloo.Images,
   FMX.ImgList,
   Olf.FMX.TextImageFrame,
   FMX.Objects;
@@ -23,12 +22,14 @@ type
   TcadLifeLevel = class(TFrame)
     Rectangle1: TRectangle;
     OlfFMXTextImageFrame1: TOlfFMXTextImageFrame;
-    Glyph1: TGlyph;
+    Image1: TImage;
     procedure FrameResized(Sender: TObject);
   private
     FLifeLevel: int64;
     procedure SetLifeLevel(const Value: int64);
   public
+    procedure RefreshImage;
+    procedure AfterConstruction; override;
     property LifeLevel: int64 read FLifeLevel write SetLifeLevel;
     constructor Create(AOwner: TComponent); override;
   end;
@@ -38,9 +39,22 @@ implementation
 {$R *.fmx}
 
 uses
-  udmAdobeStock_257147901;
+  udmAdobeStock_257147901,
+  Olf.Skia.SVGToBitmap,
+  USVGItems;
 
 { TFrame1 }
+
+procedure TcadLifeLevel.AfterConstruction;
+begin
+  inherited;
+
+  tthread.forcequeue(nil,
+    procedure
+    begin
+      RefreshImage;
+    end);
+end;
 
 constructor TcadLifeLevel.Create(AOwner: TComponent);
 begin
@@ -51,16 +65,30 @@ end;
 
 procedure TcadLifeLevel.FrameResized(Sender: TObject);
 begin
-  Glyph1.height := height - Glyph1.margins.left - Glyph1.margins.Right;
-  Glyph1.width := Glyph1.height;
+  Image1.height := height - Image1.margins.left - Image1.margins.Right;
+  Image1.Width := Image1.height;
+  RefreshImage;
+end;
+
+procedure TcadLifeLevel.RefreshImage;
+var
+  bmp: tbitmap;
+begin
+  bmp := SVGToBitmap(round(Image1.Width), round(Image1.height),
+    SVGItems[CSVGCoeur], Image1.Bitmap.bitmapscale);
+  try
+    Image1.Bitmap.Assign(bmp);
+  finally
+    bmp.free;
+  end;
 end;
 
 procedure TcadLifeLevel.SetLifeLevel(const Value: int64);
 begin
   FLifeLevel := Value;
   OlfFMXTextImageFrame1.Text := FLifeLevel.ToString;
-  width := Glyph1.margins.left + Glyph1.width + Glyph1.margins.Right +
-    OlfFMXTextImageFrame1.margins.left + OlfFMXTextImageFrame1.width +
+  Width := Image1.margins.left + Image1.Width + Image1.margins.Right +
+    OlfFMXTextImageFrame1.margins.left + OlfFMXTextImageFrame1.Width +
     OlfFMXTextImageFrame1.margins.Right;
 end;
 
