d58c16abcf8fe5569ee76b96bcf9dc01442088fb
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2024-05-26 17:46:25 +0200

    replaced local bitmap creation by the use of SVGBitmaplist with a cache

--- src-SVG-graphics-tester/fMain.pas
+++ src-SVG-graphics-tester/fMain.pas
@@ -44,9 +44,10 @@ type
     procedure btnStopClick(Sender: TObject);
     procedure Image1Resize(Sender: TObject);
   private
-    { Déclarations privées }
   public
-    PTabSVG: PTabString;
+    // PTabSVG: PTabString;
+    ListIndex, ItemIndex, MaxItemIndex: word;
+    lstBackground, lstItems, lstPersos, lstTrees, lstUserInterface: word;
   end;
 
 var
@@ -68,19 +69,46 @@ uses
 
 procedure TForm1.btnBackgroundsClick(Sender: TObject);
 begin
-  PTabSVG := @SVGBackgrounds;
+  // PTabSVG := @SVGBackgrounds;
+  if (lstBackground = 0) then
+  begin
+    lstBackground := TOlfSVGBitmapList.AddAList;
+    for var i := 0 to length(SVGBackgrounds) - 1 do
+      TOlfSVGBitmapList.AddItemAt(lstBackground, i, SVGBackgrounds[i]);
+  end;
+
+  ListIndex := lstBackground;
+  MaxItemIndex := length(SVGBackgrounds);
   Timer1.Enabled := true;
 end;
 
 procedure TForm1.btnItemsClick(Sender: TObject);
 begin
-  PTabSVG := @SVGItems;
+  // PTabSVG := @SVGItems;
+  if (lstItems = 0) then
+  begin
+    lstItems := TOlfSVGBitmapList.AddAList;
+    for var i := 0 to length(SVGitems) - 1 do
+      TOlfSVGBitmapList.AddItemAt(lstItems, i, SVGitems[i]);
+  end;
+
+  ListIndex := lstItems;
+  MaxItemIndex := length(SVGitems);
   Timer1.Enabled := true;
 end;
 
 procedure TForm1.btnPersosClick(Sender: TObject);
 begin
-  PTabSVG := @SVGPersos;
+  // PTabSVG := @SVGPersos;
+  if (lstPersos = 0) then
+  begin
+    lstPersos := TOlfSVGBitmapList.AddAList;
+    for var i := 0 to length(SVGPersos) - 1 do
+      TOlfSVGBitmapList.AddItemAt(lstPersos, i, SVGPersos[i]);
+  end;
+
+  ListIndex := lstPersos;
+  MaxItemIndex := length(SVGPersos);
   Timer1.Enabled := true;
 end;
 
@@ -91,49 +119,81 @@ end;
 
 procedure TForm1.btnTreesClick(Sender: TObject);
 begin
-  PTabSVG := @SVGTrees;
+  // PTabSVG := @SVGTrees;
+  if (lstTrees = 0) then
+  begin
+    lstTrees := TOlfSVGBitmapList.AddAList;
+    for var i := 0 to length(SVGtrees) - 1 do
+      TOlfSVGBitmapList.AddItemAt(lstTrees, i, SVGtrees[i]);
+  end;
+
+  ListIndex := lstTrees;
+  MaxItemIndex := length(SVGtrees);
   Timer1.Enabled := true;
 end;
 
 procedure TForm1.btnUserInterfaceClick(Sender: TObject);
 begin
-  PTabSVG := @SVGUserInterface;
+  // PTabSVG := @SVGUserInterface;
+  if (lstUserInterface = 0) then
+  begin
+    lstUserInterface := TOlfSVGBitmapList.AddAList;
+    for var i := 0 to length(SVGUserInterface) - 1 do
+      TOlfSVGBitmapList.AddItemAt(lstUserInterface, i, SVGUserInterface[i]);
+  end;
+
+  ListIndex := lstUserInterface;
+  MaxItemIndex := length(SVGUserInterface);
   Timer1.Enabled := true;
 end;
 
 procedure TForm1.FormCreate(Sender: TObject);
 begin
+  ListIndex := 0;
+  ItemIndex := 0;
+  MaxItemIndex := 0;
+  lstBackground := 0;
+  lstItems := 0;
+  lstPersos := 0;
+  lstTrees := 0;
+  lstUserInterface := 0;
+
   Timer1.Enabled := false;
-  Timer1.Tag := -1;
 end;
 
 procedure TForm1.Image1Resize(Sender: TObject);
 begin
-  Image1.Bitmap.SetSize(trunc(Image1.Width * Image1.Bitmap.bitmapscale),
-    trunc(Image1.Height * Image1.Bitmap.bitmapscale));
+  // Image1.Bitmap.SetSize(trunc(Image1.Width * Image1.Bitmap.bitmapscale),
+  // trunc(Image1.Height * Image1.Bitmap.bitmapscale));
+
+  TOlfSVGBitmapList.ClearCache;
 end;
 
 procedure TForm1.Timer1Timer(Sender: TObject);
-var
-  bmp: tbitmap;
+// var
+// bmp: tbitmap;
 begin
-  Timer1.Tag := Timer1.Tag + 1;
-  if (Timer1.Tag >= length(PTabSVG^)) then
-    Timer1.Tag := 0;
+  ItemIndex := ItemIndex + 1;
+  // if (ItemIndex >= length(PTabSVG^)) then
+  if (ItemIndex >= MaxItemIndex) then
+    ItemIndex := 0;
 
-  Label1.Text := Timer1.Tag.tostring;
+  Label1.Text := ItemIndex.tostring;
 
-  if Image1.Bitmap.IsEmpty then
-    Image1.Bitmap.SetSize(trunc(Image1.Width * Image1.Bitmap.bitmapscale),
-      trunc(Image1.Height * Image1.Bitmap.bitmapscale));
+  // if Image1.Bitmap.IsEmpty then
+  // Image1.Bitmap.SetSize(trunc(Image1.Width * Image1.Bitmap.bitmapscale),
+  // trunc(Image1.Height * Image1.Bitmap.bitmapscale));
 
-  bmp := SVGToBitmap(Image1.Bitmap.Width, Image1.Bitmap.Height,
-    PTabSVG^[Timer1.Tag], Image1.Bitmap.bitmapscale);
-  try
-    Image1.Bitmap.Assign(bmp);
-  finally
-    bmp.free;
-  end;
+  Image1.Bitmap.Assign(TOlfSVGBitmapList.Bitmap(ListIndex, ItemIndex,
+    round(Image1.Width), round(Image1.Height), Image1.Bitmap.bitmapscale));
+
+  // bmp := SVGToBitmap(Image1.Bitmap.Width, Image1.Bitmap.Height,
+  // PTabSVG^[ItemIndex], Image1.Bitmap.bitmapscale);
+  // try
+  // Image1.Bitmap.Assign(bmp);
+  // finally
+  // bmp.free;
+  // end;
 end;
 
 initialization
