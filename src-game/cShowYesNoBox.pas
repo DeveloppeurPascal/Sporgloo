4737acb9afb2d999bba7fced7847d2adf00846f7
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2024-06-23 16:17:43 +0200

    added a Yes/No message box

---
+++ src-game/cShowYesNoBox.pas
@@ -0,0 +1,101 @@
+unit cShowYesNoBox;
+
+interface
+
+uses
+  System.SysUtils,
+  System.Types,
+  System.UITypes,
+  System.Classes,
+  System.Variants,
+  FMX.Types,
+  FMX.Graphics,
+  FMX.Controls,
+  FMX.Forms,
+  FMX.Dialogs,
+  FMX.StdCtrls,
+  cGrayBox,
+  cYellowMenuButton,
+  FMX.Layouts,
+  FMX.Objects;
+
+type
+  TcadShowYesNoBox = class(TcadGrayBox)
+    GridPanelLayout1: TGridPanelLayout;
+    btnYes: TcadYellowMenuButton;
+    btnNo: TcadYellowMenuButton;
+    Text1: TText;
+    procedure btnNoClick(Sender: TObject);
+    procedure btnYesClick(Sender: TObject);
+  private
+  protected
+    FOnYesClick, FOnNoClick: TProc;
+    procedure SelfDestroy;
+  public
+    class procedure ShowModal(const ACurForm: TForm; const Text: string;
+      const OnYesClick: TProc = nil; const OnNoClick: TProc = nil);
+  end;
+
+implementation
+
+{$R *.fmx}
+
+procedure TcadShowYesNoBox.btnNoClick(Sender: TObject);
+begin
+  if assigned(FOnNoClick) then
+    FOnNoClick;
+  SelfDestroy;
+end;
+
+procedure TcadShowYesNoBox.btnYesClick(Sender: TObject);
+begin
+  if assigned(FOnYesClick) then
+    FOnYesClick;
+  SelfDestroy;
+end;
+
+procedure TcadShowYesNoBox.SelfDestroy;
+begin
+  tthread.ForceQueue(nil,
+    procedure
+    begin
+      if assigned(TagObject) and (TagObject is trectangle) then
+        TagObject.free;
+      self.free;
+    end);
+end;
+
+class procedure TcadShowYesNoBox.ShowModal(const ACurForm: TForm;
+const Text: string; const OnYesClick, OnNoClick: TProc);
+var
+  smb: TcadShowYesNoBox;
+  r: trectangle;
+begin
+  if not assigned(ACurForm) then
+    raise exception.Create('Specify the parent form !');
+  // TODO : intercepter ESC / RETURN (ou Joystick/télécommande) de la fiche parente pour déclencher le clic sur ce bouton
+
+  r := trectangle.Create(ACurForm);
+  r.Parent := ACurForm;
+  r.Align := TAlignLayout.contents;
+  r.Opacity := 0.6;
+  r.HitTest := true;
+  r.Stroke.Kind := tbrushkind.None;
+  r.fill.Kind := tbrushkind.Solid;
+  r.fill.Color := talphacolors.Lightgray;
+
+  smb := TcadShowYesNoBox.Create(ACurForm);
+  smb.TagObject := r;
+  smb.Parent := ACurForm;
+  smb.btnYes.txtImage.Text := 'Yes'; // TODO : traduire le texte
+  smb.FOnYesClick := OnYesClick;
+  smb.btnNo.txtImage.Text := 'No';
+  smb.FOnNoClick := OnNoClick;
+  smb.Text1.Text := Text;
+  if smb.Width > ACurForm.ClientWidth then
+    smb.Width := ACurForm.ClientWidth;
+  if smb.Height > ACurForm.ClientHeight then
+    smb.Height := ACurForm.ClientHeight;
+end;
+
+end.
