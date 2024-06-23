5725b704681ff1e2e226cad109a25d806c914094
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2024-06-23 16:42:24 +0200

    added an AutoHeight method to change default height of the box

--- src-game/cGrayBox.pas
+++ src-game/cGrayBox.pas
@@ -23,13 +23,63 @@ type
     rBackground: TRectangle;
     sContent: TVertScrollBox;
   private
-    { Déclarations privées }
   public
-    { Déclarations publiques }
+    procedure AutoHeight;
   end;
 
 implementation
 
 {$R *.fmx}
+{ TcadGrayBox }
+
+procedure TcadGrayBox.AutoHeight;
+var
+  i: integer;
+  h: single;
+  c: tcontrol;
+begin
+  h := rOutside.Margins.Top + rOutside.Margins.Bottom + rBackground.Margins.Top
+    + rBackground.Margins.Bottom;
+
+  for i := 0 to rBackground.ChildrenCount - 1 do
+    if (rBackground.Children[i] is tcontrol) then
+    begin
+      c := rBackground.Children[i] as tcontrol;
+      if c = sContent then
+        h := h + c.Margins.Top + c.Margins.Bottom
+      else
+        h := h + c.Margins.Top + c.Height + c.Margins.Bottom;
+    end;
+
+  for i := 0 to sContent.Content.ChildrenCount - 1 do
+    if (sContent.Content.Children[i] is tcontrol) then
+    begin
+      c := sContent.Content.Children[i] as tcontrol;
+      h := h + c.Margins.Top + c.Height + c.Margins.Bottom;
+    end;
+
+  if parent is tcustomform then
+  begin
+    if h > (parent as tcustomform).ClientHeight then
+      h := (parent as tcustomform).ClientHeight;
+  end
+  else if parent is tcontrol then
+  begin
+    if h > (parent as tcontrol).Height then
+      h := (parent as tcontrol).Height;
+  end;
+  Height := h;
+
+  if parent is tcustomform then
+  begin
+    if width > (parent as tcustomform).ClientWidth then
+      width := (parent as tcustomform).ClientWidth;
+  end
+  else if parent is tcontrol then
+  begin
+    if width > (parent as tcontrol).width then
+      width := (parent as tcontrol).width;
+  end;
+end;
 
 end.
